2d4c705cf0776a53e4f127168cae44d14aa575c7
tsingsun <tsingsun@live.cn>
2018-07-13 11:12:54 +0800

    fix不配置tracer而导致的异常

--- tracing/jeager.go
+++ tracing/jeager.go
@@ -3,8 +3,8 @@ package tracing
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
 	"go.uber.org/zap"
+	"github.com/uber/jaeger-client-go"
 )
 
 const GlobalSpan = "globalSpan"
@@ -20,11 +20,14 @@ func TracingField(c *gin.Context,z *zap.Logger) zap.Field {
 func (JeagerTracer) Handle() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		sban, _ := opentracing.StartSpanFromContext(c, c.Request.RequestURI)
-		tctx := sban.Context().(jaeger.SpanContext)
-		tid := tctx.TraceID().String()
-		c.Set(GlobalSpan, sban)
-		c.Set(GlobalTraceId, tid)
-		defer sban.Finish()
+		if sban != nil {
+			if tctx,ok := sban.Context().(jaeger.SpanContext);ok {
+				tid := tctx.TraceID().String()
+				c.Set(GlobalSpan, sban)
+				c.Set(GlobalTraceId, tid)
+				defer sban.Finish()
+			}
+		}
 		c.Next()
 	}
 }
